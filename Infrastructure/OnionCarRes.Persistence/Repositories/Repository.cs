09a8dfb6a4151dcc36a47830c793b84313ee69c7
Mert Kaya <merteneskaya outlook.com>
2024-04-16 18:48:02 +0300

    repository interface ve sınıfı

---
+++ Infrastructure/OnionCarRes.Persistence/Repositories/Repository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnionCarRes.Application;
+using OnionCarRes.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionCarRes.Persistence.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly CarBookContext _context;
+
+        public Repository(CarBookContext context)
+        {
+            _context = context;
+        }
+
+        public async Task CreateAsync(T entity)
+        {
+            _context.Set<T>().Add(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<T>> GetAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public Task RemoveAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
