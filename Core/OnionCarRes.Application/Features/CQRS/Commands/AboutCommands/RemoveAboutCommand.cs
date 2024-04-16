e014c1671287f06df5176d5d14ab9914043310b1
Mert Kaya <merteneskaya outlook.com>
2024-04-16 20:42:37 +0300

    about handlers tamamlandi

---
+++ Core/OnionCarRes.Application/Features/CQRS/Commands/AboutCommands/RemoveAboutCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionCarRes.Application.Features.CQRS.Commands.AboutCommands
+{
+    public class RemoveAboutCommand
+    {
+        public int Id { get; set; }
+        public RemoveAboutCommand(int id)
+        {
+            Id = id;
+        }
+    }
+}
---
+++ Core/OnionCarRes.Application/Features/CQRS/Handlers/AboutHandlers/CreateAboutCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using OnionCarRes.Application.Features.CQRS.Commands.AboutCommands;
+using OnionCarRes.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionCarRes.Application.Features.CQRS.Handlers.AboutHandlers
+{
+    public class CreateAboutCommandHandler
+    {
+        private readonly IRepository <About> _repository;
+
+        public CreateAboutCommandHandler(IRepository<About> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(CreateAboutCommand commend)
+        {
+            await _repository.CreateAsync(new About
+            {
+                Title = commend.Title,
+                Description = commend.Description,
+                ImageUrl = commend.ImageUrl
+            });
+               
+        }
+    }
+}
