656d91cf44c5e17fbbbb08181b4c3febd2542a1b
Enes Mert Kaya <merteneskaya@outlook.com>
2024-04-17 20:42:38 +0300

    UI Layout

---
+++ Frontends/OnionCarRes.WebUI/ViewComponents/UILayoutViewComponents/_FooterUILayoutComponentPartial.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+
+namespace OnionCarRes.WebUI.ViewComponents.UILayoutViewComponents
+{
+    public class _FooterUILayoutComponentPartial:ViewComponent
+    {
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
