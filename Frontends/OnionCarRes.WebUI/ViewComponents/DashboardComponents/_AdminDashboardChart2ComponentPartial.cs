349e62792120bfb1131bad00c7902bb55a28340b
Enes Mert Kaya <merteneskaya@outlook.com>
2024-04-26 17:30:25 +0300

    admin dashboard ve grafikler eklendi

---
+++ Frontends/OnionCarRes.WebUI/ViewComponents/DashboardComponents/_AdminDashboardChart2ComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnionCarRes.WebUI.ViewComponents.DashboardComponents
+{
+    public class _AdminDashboardChart2ComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
